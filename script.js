f937f530539509d5aaa9954cf4625c6c100a3156
kavi <116868545+kavita2913@users.noreply.github.com>
2023-02-04 15:44:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let num = '';
+let answer = ''
+
+function add(value) {
+    console.log(value);
+    num = num + value;
+    document.getElementById("input").value = num;
+}
+function ans() {
+    answer = eval(num);
+    document.getElementById("input").value = answer;
+    answer = '';
+    num = '';
+}
+function clr() {
+    num = '';
+    document.getElementById("input").value = "";
+}
